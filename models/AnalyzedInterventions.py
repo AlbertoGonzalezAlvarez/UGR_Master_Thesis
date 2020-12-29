515f6e5e975ac6423f99d0d9b0e406fbdd1504b4
AlberTJ97 <alu0100949568@ull.edu.es>
2020-12-29 04:02:21 +0000

    Ref: reduced dataframe columns - Specifying dataframe types to optimize memory - Some refactorization

--- models/AnalyzedInterventions.py
+++ models/AnalyzedInterventions.py
@@ -7,9 +7,18 @@ from config.AppConfig import N_TOPICS
 
 locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
+topic_idx_names = [f'topic_{i}' for i in range(0, N_TOPICS)]
+topic_time_names = [f'topic_{i}_time' for i in range(0, N_TOPICS)]
+
 # Dataframe ordered by date
-analyzed_interventions = pd.read_json('./assets/analyzed_interventions_separated_cols_w_topic_times.gzip',
-                                      compression='gzip', convert_dates=['fecha'])
+# TODO: map strings to numbers and create another df
+analyzed_interventions = pd.read_json('./assets/analyzed_interventions.gzip', compression='gzip', convert_dates=['fecha'])
+analyzed_interventions = analyzed_interventions.convert_dtypes()
+
+float_downcast_columns = topic_idx_names + topic_time_names
+analyzed_interventions[float_downcast_columns] = analyzed_interventions[float_downcast_columns].apply(
+	pd.to_numeric, downcast='float')
+
 
 def get_parties_info():
 	n_interventions = analyzed_interventions['organizacion'] \
@@ -35,33 +44,30 @@ def get_parties_info():
 
 
 def topic_distribution_per_party():
-	return analyzed_interventions[[f'topic_{i}' for i in range(0, N_TOPICS)] + ['organizacion']].groupby(
+	return analyzed_interventions[topic_idx_names + ['organizacion']].groupby(
 		'organizacion').mean()
 
 
 def topic_distribution_per_sex():
-	return analyzed_interventions[[f'topic_{i}' for i in range(0, N_TOPICS)] + ['sexo']].groupby('sexo').mean()
+	return analyzed_interventions[topic_idx_names + ['sexo']].groupby('sexo').mean()
 
 
 def max_time_per_topic():
 	return analyzed_interventions[
-		['predominant_topic', 'organizacion', 'intervention_time', 'diputado', 'fecha'] +
-		[f'topic_{i}_time' for i in range(0, N_TOPICS)]
-		].iloc[analyzed_interventions[[f'topic_{i}_time' for i in range(0, N_TOPICS)]].idxmax()]
+		['predominant_topic', 'organizacion', 'intervention_time', 'diputado', 'fecha'] + topic_time_names]\
+			.iloc[analyzed_interventions[topic_time_names].idxmax()]
 
 
 def monthly_topics(computation_function):
 	date_range = analyzed_interventions.fecha.dt.strftime("%B-%Y").transform(string.capwords).unique()
-	data_columns = {
-		**{f'topic_{i}': 0 for i in range(0, N_TOPICS)}
-	}
+	data_columns = dict.fromkeys(topic_idx_names, 0)
 
 	monthly_topics_scores = analyzed_interventions.groupby(
 		[
 			analyzed_interventions.fecha.dt.strftime("%B-%Y").transform(string.capwords),
 			'organizacion'
 		]
-		, sort=False)[[f'topic_{i}' for i in range(0, N_TOPICS)]].apply(computation_function).reset_index()
+		, sort=False)[topic_idx_names].apply(computation_function).reset_index()
 
 	party_topic_scores_per_month = monthly_topics_scores.groupby('organizacion', sort=False)
 	party_monthly_data = {}
