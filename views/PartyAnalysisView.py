13e1d55aa0813006b53470b4ade3cb01373f37e5
AlberTJ97 <alu0100949568@ull.edu.es>
2021-01-11 19:05:35 +0000

    Ref: Changed imports

--- views/PartyAnalysisView.py
+++ views/PartyAnalysisView.py
@@ -2,8 +2,8 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 
-import WebApplication
 import controllers
+from WebApplication import is_mobile_device
 from config.AppConfig import PARTY_CONFIG
 from models import AnalyzedInterventions
 
@@ -75,7 +75,7 @@ party_info_card_content = [
 				'type': 'collapsible-item',
 				'ref': party
 			},
-			is_open=not WebApplication.is_mobile_device,
+			is_open=not is_mobile_device,
 		)
 	] for party in PARTY_CONFIG
 ]
