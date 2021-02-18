aed690b167ae456c49f316c1283e2cf51d8e1284
AlberTJ97 <alu0100949568@ull.edu.es>
2021-02-18 00:38:45 +0000

    Ref: improved and some code refactorization - Added lux template to all figures

--- controllers/PageControllers/PartyAnalysisController.py
+++ controllers/PageControllers/PartyAnalysisController.py
@@ -3,34 +3,37 @@ import json
 import dash_bootstrap_components as dbc
 import numpy as np
 from dash import callback_context
-from dash.dependencies import Output, Input, MATCH, ALL
+from dash.dependencies import Output, Input, MATCH, ALL, State
 from plotly import graph_objs as go
 
 import controllers
 from WebApp import WebApp
-from config.AppConfig import PARTY_CONFIG, TOPIC_NAMES, PLOT_BASE_CONFIG, SEX_CONFIG, N_TOPICS
+from config.AppConfig import PARTY_CONFIG, TOPIC_NAMES, SEX_CONFIG, N_TOPICS
 from models.AnalyzedInterventions import topic_distribution_per_party, topic_distribution_per_sex, max_time_per_topic, \
 	monthly_topics
 
 
 def build_topic_chart():
 	data = topic_distribution_per_party()
-	figure = go.Figure(data=[
-		go.Bar(
-			x=TOPIC_NAMES,
-			y=data.loc[data.index == party_id].values.squeeze(),
-			name=party_id,
-			marker_color=PARTY_CONFIG[party_id]['color'],
-			showlegend=False if controllers.MainController.is_mobile_device else True,
-			hovertemplate='Porcentaje de tiempo: %{y:.2f}<br>'
-		) for party_id in PARTY_CONFIG],
-		layout=dict(
-			barmode='group',
-			margin=dict(l=0, r=40, t=40),
-			xaxis_title='' if controllers.MainController.is_mobile_device else 'Temas',
-			yaxis_title='' if controllers.MainController.is_mobile_device else 'Porcentaje de tiempo',
-			**PLOT_BASE_CONFIG
+	figure = go.Figure()
+
+	for party_id in PARTY_CONFIG:
+		figure.add_trace(
+			go.Bar(
+				x=list(TOPIC_NAMES.keys()),
+				y=data.loc[data.index == party_id].values.squeeze(),
+				name=party_id,
+				marker_color=PARTY_CONFIG[party_id]['color'],
+				hovertemplate='Porcentaje de tiempo: %{y:.2f}<br>',
+			)
 		)
+
+	figure.layout.update(
+		template='lux',
+		barmode='group',
+		showlegend=False,
+		xaxis_title='' if controllers.MainController.is_mobile_device else 'Temas',
+		yaxis_title='' if controllers.MainController.is_mobile_device else 'Porcentaje de tiempo',
 	)
 
 	return figure
@@ -38,22 +41,25 @@ def build_topic_chart():
 
 def build_woman_vs_man_chart():
 	data = topic_distribution_per_sex()
-	figure = go.Figure(data=[
-		go.Bar(
-			x=TOPIC_NAMES,
-			y=data.loc[data.index == sex_id].values.squeeze(),
-			name=SEX_CONFIG[sex_id]['name'],
-			marker_color=SEX_CONFIG[sex_id]['color'],
-			showlegend=False if controllers.MainController.is_mobile_device else True,
-			hovertemplate='Porcentaje de tiempo: %{y:.2f}<br>'
-		) for sex_id in SEX_CONFIG],
-		layout=dict(
-			barmode='group',
-			margin=dict(l=0, r=40, t=40),
-			xaxis_title='' if controllers.MainController.is_mobile_device else 'Temas',
-			yaxis_title='' if controllers.MainController.is_mobile_device else 'Porcentaje de tiempo',
-			**PLOT_BASE_CONFIG
+	figure = go.Figure()
+
+	for sex_id in SEX_CONFIG:
+		figure.add_trace(
+			go.Bar(
+				x=list(TOPIC_NAMES.keys()),
+				y=data.loc[data.index == sex_id].values.squeeze(),
+				name=SEX_CONFIG[sex_id]['name'],
+				marker_color=SEX_CONFIG[sex_id]['color'],
+				showlegend=False if controllers.MainController.is_mobile_device else True,
+				hovertemplate='Porcentaje de tiempo: %{y:.2f}<br>'
+			)
 		)
+
+	figure.layout.update(
+		template='lux',
+		barmode='group',
+		xaxis_title='' if controllers.MainController.is_mobile_device else 'Temas',
+		yaxis_title='' if controllers.MainController.is_mobile_device else 'Porcentaje de tiempo',
 	)
 
 	return figure
@@ -61,96 +67,72 @@ def build_woman_vs_man_chart():
 
 def build_topic_chart_duration():
 	data = max_time_per_topic()
-	figure = go.Figure(go.Bar(
-			x=data['predominant_topic'],
+	figure = go.Figure(
+		go.Bar(
+			x=list(TOPIC_NAMES.keys()),
 			y=list(np.diag(data[[f'topic_{i}_time' for i in range(0, N_TOPICS)]])),
-			marker_color=controllers.COLORS,
-			customdata=list(zip(
-				data['fecha'], data['diputado'].apply(controllers.camel_case_deputy_name), data['organizacion'])),
-			hoverinfo='skip',
+			marker_color=list(TOPIC_NAMES.values()),
+			customdata=list(zip( data['fecha'], data['diputado'].apply(str.title), data['organizacion'])),
 			hovertemplate=
 			'<b>Tema: %{x}</b><br><br>' +
-
 			'Fecha: %{customdata[0]|%d-%m-%y}<br>' +
 			'Diputado: %{customdata[1]}<br>' +
 			'Partido: %{customdata[2]}<br>' +
 			'Duración: %{y:.0f} min <br>' +
 			'<extra></extra>'
-		),
-		layout=dict(
-			barmode='group',
-			hoverlabel_align='left',
-			margin=dict(l=0, r=40, t=40),
-			xaxis_title='' if controllers.MainController.is_mobile_device else 'Temas',
-			yaxis_title='' if controllers.MainController.is_mobile_device else 'Minutos estimados',
-			**PLOT_BASE_CONFIG
 		)
+	)
+
+	figure.layout.update(
+		template='lux',
+		barmode='group',
+		xaxis_title='' if controllers.MainController.is_mobile_device else 'Temas',
+		yaxis_title='' if controllers.MainController.is_mobile_device else 'Minutos estimados',
 	)
 
 	return figure
 
 
-def topic_scores_per_month(party, topics):
+def topic_scores_per_month():
 	monthy_topic_scores_per_party = monthly_topics()
 	figure = go.Figure()
 
-	for topic_idx in topics:
-		figure.add_trace(
-			go.Scatter(
-				x=monthy_topic_scores_per_party[party].index,
-				y=monthy_topic_scores_per_party[party][f'topic_{topic_idx}'],
-				text=monthy_topic_scores_per_party[party].index,
-				name=TOPIC_NAMES[topic_idx],
-				yaxis=f'y',
-				visible=True,
-				line=dict(color=controllers.get_color(topic_idx)),
-				hovertemplate=
-				"Mes: %{x}<br>" +
-				"Media de minutos: %{y:.3f}<br>"
+	for party_id in monthy_topic_scores_per_party:
+		for topic_idx, topic_name in enumerate(TOPIC_NAMES):
+			figure.add_trace(
+				go.Scatter(
+					x=monthy_topic_scores_per_party[party_id].index,
+					y=monthy_topic_scores_per_party[party_id][f'topic_{topic_idx}'],
+					text=monthy_topic_scores_per_party[party_id].index,
+					line=dict(color=TOPIC_NAMES[topic_name]),
+				)
 			)
-		)
 
 	figure.update_traces(
-		hoverinfo="name+x+text+y",
-		line={"width": 1},
-		marker={"size": 10},
-		mode="lines+markers",
-		showlegend=False
+		mode='lines+markers',
+		showlegend=False,
+		visible=False,
+		hovertemplate=
+		'Mes: %{x}<br>' +
+		'Media de minutos: %{y:.3f}<br>'
+		'<extra></extra>',
 	)
 
-	# Update axes
 	figure.layout.update(
+		template='lux',
+		hovermode="x",
 		xaxis=dict(
-			autorange=True,
-			rangeslider=dict(
-				autorange=True,
-			),
+			rangeslider=dict(visible=True),
 		),
 		yaxis=dict(
-			anchor="x",
-			autorange=True,
-			domain=[0, 1],
-			mirror=True,
-			showline=True,
-			side="right",
-			tickfont={"color": "#673ab7"},
-			tickmode="auto",
-			titlefont={"color": "#673ab7"},
-			type="linear",
-			zeroline=False
+			side='right',
+			zeroline=False,
 		),
-	)
-
-	# Update layout
-	figure.layout.update(
-		dragmode="zoom",
-		hovermode="x",
-		yaxis_title="Minutos",
-		legend=dict(traceorder="reversed"),
+		yaxis_title='Minutos',
 		height=1000,
 		margin=dict(
-			t=100,
-			b=100
+			t=50,
+			b=20
 		),
 	)
 
@@ -160,9 +142,10 @@ def topic_scores_per_month(party, topics):
 @WebApp.callback(
 	Output({'id': 'dd-parties-div', 'loc': MATCH}, 'children'),
 	Input({'type': 'dd-party', 'loc': MATCH, 'ref': ALL}, 'n_clicks'),
+	State({'id': 'monthly-data', 'loc': MATCH}, 'figure'),
 	prevent_initial_call=True
 )
-def update_party_dropdown(_):
+def update_party_dropdown(_, figure_status):
 	fired_prop_id = callback_context.triggered[0]['prop_id']
 	selected_party = fired_prop_id.split('.')[0]
 	data = json.loads(selected_party)
@@ -195,36 +178,28 @@ def update_party_dropdown(_):
 
 @WebApp.callback(
 	Output({'id': 'monthly-data', 'loc': MATCH}, 'figure'),
+	Output({'id': 'monthly-data', 'loc': MATCH}, 'style'),
+	Output({'id': 'select-data', 'loc': MATCH}, 'style'),
+	Output({'id': 'warning', 'loc': MATCH}, 'style'),
 	Input({'id': 'dd-topics', 'loc': MATCH}, 'value'),
 	Input({'id': 'dd-parties', 'loc': MATCH}, 'label'),
+	State({'id': 'monthly-data', 'loc': MATCH}, 'figure'),
 )
-def update_monthly_graph(topics, actual_party):
+def update_monthly_graph(topics, actual_party, figure):
 	if topics and actual_party in PARTY_CONFIG:
-		return topic_scores_per_month(actual_party, topics)
+		party_scatter_start = len(TOPIC_NAMES) * list(PARTY_CONFIG).index(actual_party)
 
-	figure = go.Figure()
-	figure.layout.update(
-		{
-			"xaxis": {
-				"visible": False
-			},
-			"yaxis": {
-				"visible": False
-			},
-			"annotations": [
-				{
-					"text": "Selecciona un partido y al menos un tema",
-					"xref": "paper",
-					"yref": "paper",
-					"showarrow": False,
-					"font": {
-						"size": 28
-					}
-				}
-			],
-			"height": 1000,
-			"margin": dict(t=100, b=100)
-		}
-	)
+		for idx in range(len(figure['data'])):
+			figure['data'][idx]['visible'] = False
 
-	return figure
+		for topic_idx in topics:
+			idx = topic_idx + party_scatter_start
+			figure['data'][idx]['visible'] = True
+
+		figure['layout'].update(
+			plot_bgcolor=controllers.rgb_to_rgba(PARTY_CONFIG[actual_party]['color'], 0.1)
+		)
+
+		return figure, dict(display='block'), dict(display='none'), dict(display='none')
+
+	return figure, dict(display='none'), dict(display='block'), dict(display='block')
